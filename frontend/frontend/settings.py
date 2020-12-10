d0588f7335d91ec21c6e2a038b63b6cc9e325d97
Grace <gracek1459@gmail.com>
2020-12-09 23:15:26 -0500

    fixed something minor

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h)ogrc__wfc9njzw1puy7lo8jw&(c55jz&10(^@e6fe8m!ffz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
